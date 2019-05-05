623d83d1a1dca119c98300ad509e5df2d691b0d3
shola <sholafalana@ymail.com>
2019-05-05 11:28:18 +0100

    create a database access object class for the movies entitiy

--- app/src/main/java/com/example/popularmoviesapp2/mvvm/datasource/MovieDao.java
+++ app/src/main/java/com/example/popularmoviesapp2/mvvm/datasource/MovieDao.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @Dao
 public interface MovieDao {
+    //TODO: create a database access object class for the movies entitiy
     @Query("SELECT * FROM movies WHERE sortby=:sortby")
     List<MovieData> getAll(String sortby);
 
